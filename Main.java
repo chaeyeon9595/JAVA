dc9bacb8e6728f344cb63630888b752c15837dab
chaeyeon9595 <cjang5189@gmail.com>
2025-03-27 21:46:21 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,35 @@
 package org.example;
 
-// 문제 : 아래 코드가 실행되도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        new 자동차().달리다(); //일회용 객체
-        new 자동차().달리다();
-        new 자동차().달리다(); // -->3번 달린 것? 아니다..
-        //1번 만들어지고 실행되고 소멸되고 또 만들어지고 실행되고 소멸되고.... 그렇게 3번 반복! 한 자동차가 3번 달린 것 아니다.
+        Cal.plus_print(10, 20);
+        // 출력 => 결과 : 30
+
+        Cal.plus_print(50, 20);
+        // 출력 => 결과 : 70
+
+        Cal.minus_print(50, 20);
+        // 출력 => 결과 : 30
+
+        Cal.minus_print(5, 2);
+        // 출력 => 결과 : 3
+
+        Cal.multi_print(5, 2);
+        // 출력 => 결과 : 10
     }
 }
 
-class 자동차{
-    void 달리다(){
-        System.out.println("달리다");
+class Cal {
+    public static void plus_print(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void minus_print(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public static void multi_print(int a, int b) {
+        System.out.println(a * b);
     }
 }
