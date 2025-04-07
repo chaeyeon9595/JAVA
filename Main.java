0ed53ee9de48227d5374a0ab297b22828d372864
chaeyeon9595 <cjang5189@gmail.com>
2025-04-07 21:46:27 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,47 +1,85 @@
 package org.example;
 
+// 문제 : 아래가 실행되도록 해주세요.
+// 조건 : 배열을 사용할 수 없습니다.
 
 class Main {
     public static void main(String[] args) {
+        사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
+        a사람인력관리소.add사람("홍길순", 33);
+        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
+        a사람인력관리소.add사람("홍길동", 20);
+        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
+        a사람인력관리소.add사람("임꺽정", 30);
+        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
 
-        Article article1 = new Article();
-        System.out.println(article1.lastID); // 1
+        사람 a사람1 = a사람인력관리소.get사람(1);
+        a사람1.자기소개();
+        // 저는 1번, 홍길순, 33살 입니다.
 
+        사람 a사람2 = a사람인력관리소.get사람(2);
+        a사람2.자기소개();
+        // 저는 2번, 홍길동, 20살 입니다.
 
-        Article article2= new Article();
-        System.out.println(article2.lastID); // 2
-
-        Article article3= new Article();
-        System.out.println(article3.lastID); // 3
-
-        Article article4= new Article();
-        System.out.println(article4.lastID); // 3
-
-        Article article5= new Article();
-        System.out.println(article5.lastID); // 3
-
-        Article article6= new Article();
-        System.out.println(article6.lastID); // 3
-
-
+        사람 a사람3 = a사람인력관리소.get사람(3);
+        a사람3.자기소개();
+        // 저는 3번, 임꺽정, 30살 입니다.
     }
-
 }
 
-class Article{
-    int id;
-    String regDate;
-    static int lastID = 0;
+class 사람인력관리소 {
+    사람 a사람;
+    사람 b사람;
+    사람 c사람;
+    int lastId = 0;
 
-    Article() {
-        this(lastID + 1, "2025-4-2");
-        lastID++;
-        //this메소드 : 객체(). 나 자신을 실행함. 생성자.
+
+    void add사람(String name, int age) {
+        사람 temp = new 사람();
+        lastId++;
+        temp.id = lastId;
+        temp.name = name;
+        temp.age = age;
+
+        if (lastId == 1) {
+            a사람 = temp;
+        }
+        if (lastId == 2) {
+            b사람 = temp;
+        }
+        if (lastId == 3) {
+            c사람 = temp;
+        }
+        System.out.println("나이가 " + temp.age + "살인 " + temp.id + "번째 사람(" + temp.name + ")이 추가되었습니다.");
+
+
     }
 
-    Article(int id, String regDate){
-        this.id = id;
-        this.regDate = regDate;
+    사람 get사람(int i) {
+
+        if (i == 1) {
+            return a사람;
+        }
+        if (i == 2) {
+            return b사람;
+        }
+        if (i == 3) {
+            return c사람;
+        }
+
+        return null;
+    }
+}
+
+
+class 사람{
+    int id;
+    String name;
+    int age;
+
+    void 자기소개(){
+        System.out.println("저는 " + this.id + "번," + this.name + "," + this.age + "살입니다.");
+
     }
 }
